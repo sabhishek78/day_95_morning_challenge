d478084c6b766e2c895dcf0accffeaf73c599672
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2020-03-09 16:59:21 +0530

    finished

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 // Challenge 1
 // Longest Streak
 // Create a function that takes an array of objects with date property and return the
@@ -24,7 +25,35 @@
 //   "date": "2019-09-30"
 //   }
 // ]) ➞ 3
+int longestStreak(List dates){
+  if(dates.length==1){
+    return 1;
+  }
+  if(dates.length==0){
+    return 0;
+  }
+  dates.sort((a, b) => a['date'].compareTo(b['date']));
+  print(dates);
+  int count=1;
+  List counts=[];
+  List dateTimes=[];
+  for(int i=0;i<dates.length;i++){
+    DateTime myDatetime = DateTime.parse(dates[i]['date']);
+    dateTimes.add(myDatetime);
 
+  }
+  for(int i=0;i<dateTimes.length-1;i++){
+    final difference = dateTimes[i+1].difference(dateTimes[i]).inDays;
+
+    if(difference==1){
+      count++;
+    }else{
+      counts.add(count);
+      count=0;
+    }
+  }
+  return counts.reduce((curr, next) => curr > next? curr: next);
+}
 
 // Challenge 2
 // This problem was asked by Microsoft.
@@ -40,6 +69,57 @@
 // Given the set of words 'bed', 'bath', 'bedbath', 'and', 'beyond', and the
 // string "bedbathandbeyond", return either ['bed', 'bath', 'and', 'beyond] or
 // ['bedbath', 'and', 'beyond'].
+List<String>Dictionary(List<String> dictionary,String word){
+
+  if(word.isEmpty){
+    return [];
+  }
+  for(int i=1;i<=word.length;i++){
+    String testString=word.substring(0,i);
+
+    if(dictionary.contains(testString)){
+      var result=Dictionary(dictionary, word.substring(i));
+      if(result!=null){
+        return result..add(testString);
+      }
+    }
+  }
+
+  return null;
+
+}
+
 
 void main() {
+  print(longestStreak([
+    {
+      "date": "2019-09-18"
+    },
+    {
+      "date": "2019-09-19"
+    },
+    {
+      "date": "2019-09-20"
+    },
+    {
+      "date": "2019-09-26"
+    },
+    {
+      "date": "2019-09-27"
+    },
+    {
+      "date": "2019-09-30"
+    }
+  ]));
+  print(longestStreak([
+    {
+      "date": "2019-09-18"
+    },
+
+  ]));
+  print(longestStreak([]));
+ print(Dictionary(['quick', 'brown', 'the', 'fox'],'thequickbrownfox'));
+ print(Dictionary(['bed', 'bath', 'bedbath', 'and', 'beyond'],'bedbathandbeyond'));
+ print(Dictionary(['bed', 'bath', 'bedbath', 'and', 'beyond'],'bedbatandbeyond'));
+ print(Dictionary(['bedbat','bedbath', 'and', 'beyond'],'bedbathandbeyond'));
 }
